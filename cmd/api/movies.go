cd3e0e60f13987cd33d86a1de583e1597ac4322b
Adrian Escat <adrian.escat@gmail.com>
2023-02-21 22:08:47 -0300

    Custom JSON decoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,23 +9,19 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Genres  []string     `json:"genres"`
 	}
 
-	// Use the new readJSON() helper to decode the request body into the input struct.
-	// If this returns an error we send the client the error message along with a 400
-	// Bad Request status code, just like before.
 	err := app.readJSON(w, r, &input)
 
 	if err != nil {
-		// Use the new badRequestResponse() helper.
 		app.badRequestResponse(w, r, err)
 		return
 	}
-
+	
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
