2c7befa9bbfbf29e3b96565d2690cee29337a35e
Adrian Escat <adrian.escat@gmail.com>
2023-02-21 22:30:55 -0300

    Validating JSON input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"greenlight.adrianescat.com/internal/data"
+	"greenlight.adrianescat.com/internal/validator"
 	"net/http"
 	"time"
 )
@@ -11,17 +12,35 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	var input struct {
 		Title   string       `json:"title"`
 		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Runtime data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
-
+	
 	err := app.readJSON(w, r, &input)
 
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
